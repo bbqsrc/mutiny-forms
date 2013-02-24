1e06da7b5b0edb60aa7989ccc0f7c6ff1321f863
Brendan Molloy <brendan@bbqsrc.net>
2013-02-24 15:59:37 +1100

    updates to the form

--- static/join.js
+++ static/join.js
@@ -2,16 +2,40 @@ angular.module("joinForm", []);
 
 function JoinCtrl($scope) {
     $scope.submit = function() {
-        var prop,
+        var prop, node,
             out = {};
+        
+        $(".ng-pristine").removeClass("ng-pristine").addClass("ng-dirty");
+
         for (prop in $scope) {
-            if (/^[\$\_]/.test(prop)) {
+            if (/^[\$\_]/.test(prop) || prop == "this" || prop == "submit") {
                 continue;
             }
             out[prop] = $scope[prop];
         }
+
+        node = $("input.ng-invalid-required").first();
+        if (node.length) {
+            node.focus();
+        } else {
+            // Do ajax
+            $.ajax(location.pathname, {
+                method: "post",
+                data: {"data": JSON.stringify(out)},
+            }).fail(function() {
+                $("#completed").find(".alert").removeClass('alert-info').addClass('alert-danger')
+                    .html("For some reason, the form had an error. Please contact the Secretary for assistance.")
+            }).always(function(data) {
+                $("form").addClass('hidden');
+                $("#completed").removeClass('hidden');
+            });
+        }
+        console.log(node);
         console.log(out);
+
+
     }
 
-    $scope.membershipType = "full";
+    $scope.membership_level = "full";
+    $scope.payment_method = "paypal";
 }
